4376301c3cc384b3b70824215fcfdc88584669fe
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2022-05-25 01:42:32 -0600

    Se agrego servicio para consumo de datos

---
+++ src/app/bad115-api-core.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, filter, tap, map, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Bad115ApiCoreService {
+  
+  readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json; charset=utf-8',
+      //Authorization: 'my-auth-token'
+    }),
+  };
+  //https://localhost:44383/api/Requisicion/Obtener
+  readonly bad115ApiCoreUrl = "https://localhost:44383/api";
+  constructor( private http:HttpClient) {}
+
+  //REQUISICION
+  body=JSON.stringify({"idrequisicion": "1"});
+   getListaRequisiciones():Observable<any>{
+    return this.http.post(this.bad115ApiCoreUrl + "/Requisicion/Obtener/",this.body, this.httpOptions);
+    
+  }
+   //TEST-.
+   getgetListaRequisiciones(){
+     console.log("getgetListaRequisiciones()");
+     return this.http.post(this.bad115ApiCoreUrl + "/Requisicion/Obtener", "", this.httpOptions)
+     .subscribe(data => console.log(data));
+   }
+/*
+   addRequisicion(data:any){
+     return this.http.post(this.bad115ApiCoreUrl + "/Requisicion/Guardar", data);
+   }
+*/
+   //DETALLE REQUISICION
+  
+ getListaDetallesRequisicion():Observable<any>{
+    return this.http.post<any>(this.bad115ApiCoreUrl + "/Detallerequisicion/Obtener", "", this.httpOptions);
+  }
+
+  //PRODUCTOS
+  getListaProductos():Observable<any>{
+    return this.http.post<any>(this.bad115ApiCoreUrl + "/Producto/Obtener", "", this.httpOptions);
+  }
+
+  //SUCURSALES
+  getListaSucursales():Observable<any>{
+    return this.http.post<any>(this.bad115ApiCoreUrl + "/Sucursal/Obtener", "", this.httpOptions);
+  }
+}
